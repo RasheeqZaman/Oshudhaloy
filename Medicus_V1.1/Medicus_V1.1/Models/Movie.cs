c0901ca2d83f3ec3deec1fef6509ed2611b2a698
RasheeqZaman <zamanrshq@gmail.com>
2017-11-26 06:55:05 +0600

    Demo Tutorial try kortisi
    Orginal App er Kaaj ekhono shuru kori nai

---
+++ Medicus_V1.1/Medicus_V1.1/Models/Movie.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Medicus_V1._1.Models
+{
+    public class Movie
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+}
