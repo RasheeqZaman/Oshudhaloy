7094ec1712b239d921b650df3afc12982f93d85e
IronmanBD <nahidul19967@gmail.com>
2018-01-12 10:12:50 +0600

    Shob html file add korsi

---
+++ Pranto/Medicus_V1.5/Medicus_V1.5/Controllers/HomeController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Medicus_V1._5.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Table()
+        {
+            return View();
+        }
+        public ActionResult Button()
+        {
+            return View();
+        }
+        public ActionResult Chart()
+        {
+            return View();
+        }
+        public ActionResult Calender()
+        {
+            return View();
+        }
+        public ActionResult Form()
+        {
+            return View();
+        }
+        public ActionResult Gallery()
+        {
+            return View();
+        }
+        public ActionResult Panel()
+        {
+            return View();
+        }
+        public ActionResult General()
+        {
+            return View();
+        }
+        public ActionResult ToDoList()
+        {
+            return View();
+        }
+        public ActionResult ResponsiveTable()
+        {
+            return View();
+        }
+    }
+}
