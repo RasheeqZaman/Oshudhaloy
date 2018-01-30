fcd0ddac4aad768a709ac903801aedb4e9c0bcf7
RasheeqZaman <zamanrshq@gmail.com>
2018-01-30 15:06:34 +0600

    dashboard er kaaj iktu baki ase

--- Medicus_V1.6.1/Medicus_V1.6.1/Controllers/AdminController.cs
+++ Medicus_V1.6.1/Medicus_V1.6.1/Controllers/AdminController.cs
@@ -19,7 +19,23 @@ namespace Medicus_V1._6._1.Controllers
 
         public ActionResult DashBoard()
         {
-            return View();
+            var data = new AdminDashBoardViewData();
+            data.todaysTotalCustomer = db.Database.SqlQuery<int>("SELECT COUNT(CustomerId) FROM CustomerOrders inner join Admins on Admins.pharmacyId = CustomerOrders.pharmacyId WHERE Admins.adminId = "+ Session["ID"] + " and OrderDate = '"+DateTime.Today+"'").FirstOrDefault();
+            //data.todaysTotalSale = db.Database.SqlQuery<int>("SELECT SUM(Medicines.SellPrice*CustomerOrders.Quantity) FROM CustomerOrders INNER JOIN Medicines ON CustomerOrders.MedicineId = Medicines.MedicineId INNER JOIN CustomerCarts ON CustomerOrders.CustomerOrderId = CustomerCarts.CustomerOrderId INNER JOIN PharmacySolds ON PharmacySolds.CustomerCartId = CustomerCarts.CCartId Inner join Admins on Admins.PharmacyId = CustomerOrders.PharmacyId WHERE Admins.adminId = "+Session["ID"]+" and SoldDate = '"+DateTime.Today +"'").FirstOrDefault(); ;
+            data.todaysTotalSale = 0;
+            //data.todaysTotalPurchase = db.Database.SqlQuery<int>("SELECT sum(Medicines.supplierPrice*PharmacyReceiveds.Quantity) FROM PharmacyReceiveds inner join Medicines on Medicines.MedicineId = PharmacyReceiveds.MedicineId inner join Admins on Admins.pharmacyId = CustomerOrders.pharmacyId WHERE Admins.adminId = " + Session["ID"] + " and PharmacyReceiveds.ReceivedDate = '" + DateTime.Today + "'").FirstOrDefault();
+            data.todaysTotalPurchase = 0;
+            //data.totalMedicine = db.Database.SqlQuery<int>("SELECT SUM(Quantity) FROM PharmacyReceiveds Inner join Admins on Admins.PharmacyId = PharmacyReceiveds.PharmacyId WHERE Admins.adminid = "+Session["ID"]+" and ReceivedDate = '"+DateTime.Today+"'").FirstOrDefault();
+            string check =  db.Database.SqlQuery<int?>("SELECT SUM(Quantity) FROM PharmacyReceiveds where PharmacyId = 15").FirstOrDefault().ToString();
+            if (check == null)
+            {
+                data.totalMedicine = 0;
+            }else
+            {
+                data.totalMedicine = Int32.Parse(check);
+            }
+            //data.totalMedicine = 0;
+            return View(data);
         }
 
         public ActionResult POS()
---
+++ Medicus_V1.6.1/Medicus_V1.6.1/ViewData/AdminDashBoardViewData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Medicus_V1._6._1.ViewData
+{
+    public class AdminDashBoardViewData
+    {
+        public int todaysTotalCustomer { get; set; }
+        public int totalMedicine { get; set; }
+        public int todaysTotalSale { get; set; }
+        public int todaysTotalPurchase { get; set; }
+
+    }
+}
